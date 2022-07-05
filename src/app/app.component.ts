894842f27b26789ef44c4dfe506388dd02d963c5
anic4cio <anicaciovinicius36@gmail.com>
2022-07-05 13:42:15 -0300

    [feat] - building chat component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'vaptchat';
+  // title = 'vaptchat';
+  conversation;
+
+  onConversationSelected(conversation: any) {
+    this.conversation = conversation;
+    
+  }
 }
